6ee74332fef37bf69a38b412dc12f6056f96910b
Emmanuel Sampaio <emmanuelsampaio@alu.ufc.br>
2019-08-13 12:23:48 -0300

    Funções da biblioteca

--- bitlib.c
+++ bitlib.c
@@ -1,2 +1,13 @@
 #inlcude "bitlib.h"
+void SetBit(unsigned long *p, unsigned char bitn) {    }
+
+void ResetBit(unsigned long *p, unsigned char bitn) {    }
+
+void ToggletBit(unsigned long *p, unsigned char bitn) {    }
+
+unsigned char  ReadBit(unsigned long *p, unsigned char bitn) {    }
+
+unsigned long  ReadBitSlice(unsigned long *p, unsigned char bitstart, unsigned char bitend) {    }
+
+unsigned char IsLittleEndian (void) { }
 
