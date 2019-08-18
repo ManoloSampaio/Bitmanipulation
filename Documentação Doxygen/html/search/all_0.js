6f57b37519385fc5350c1164008a4fea68fea383
Emmanuel Sampaio <emmanuelsampaio@alu.ufc.br>
2019-08-18 10:31:36 -0300

    Arquivos deletados

---
+++ Documentação Doxygen/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['binlib_2ec_0',['binlib.c',['../binlib_8c.html',1,'']]]
+];
---
+++ Documentação Doxygen/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['description_3a_19',['Description:',['../index.html',1,'']]]
+];
